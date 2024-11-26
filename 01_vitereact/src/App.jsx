3c6ced7317cbec1991e6d3bcfc5073ecdd11df18
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-11-26 19:00:33 +0000

    own library

--- 01_vitereact/src/App.jsx
+++ 01_vitereact/src/App.jsx
@@ -1,12 +1,12 @@
 import Coffee from "./coffee"
 
 function App() {
-  
+  const akul="adarsh Chaudhary"
   return (
     <>
     <Coffee/>
      <h1>React with vite  | Adarsh</h1>
-     <p>This is awesome</p>
+     <p>This is awesome {akul}  </p>
      </> 
   )
 }
--- 01_vitereact/src/main.jsx
+++ 01_vitereact/src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// const anotherElement = (
+//    <a href="https://google.com" target='_blank'>Visit google</a>
+// )
+const anotheruser="bhai hai"
+const reactElement = React.createElement(
+    'a',
+    {href: 'https://google.com',target: '_blank' },
+    'click me to visit google',
+    anotheruser
 )
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ 
+    reactElement
+    // <App/>
+  
+)
+
