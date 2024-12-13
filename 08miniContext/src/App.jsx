4b78ffce5c939d6780112d33fcf9bb6a226cdd4a
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-12-13 19:38:00 +0000

    toogle theme

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import UserContextProvider from './context/UserContextProvider'
+import Profile from './components/Profile'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +9,8 @@ function App() {
   return (
     <UserContextProvider>
       <h1>Adarsh Chaudhary is here</h1>
+      <Login/>
+      <Profile/>
     </UserContextProvider>
   )
 }
