07cdfb409ca725727c8ea1321cbf4068e0bce9f8
adarshchaudhary100 <147192892+adarshchaudhary100@users.noreply.github.com>
2024-12-19 16:32:37 +0000

    toggle complete

--- 09themeSwitcher/src/components/Card.jsx
+++ 09themeSwitcher/src/components/Card.jsx
@@ -4,7 +4,7 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://i.ibb.co/0JJTpZB/IMG-2773.jpg" alt="product_image1" />
+                <img className="p-8 rounded-t-lg" src="https://i.ibb.co/yPck0Vy/IMG-2788.jpg" alt="product_image1" />
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
@@ -12,7 +12,7 @@ export default function Card() {
                         Ayodhya Trip
                     </h5>
                 </a>
-                <div className="flex items-center mt-2.5 mb-5">
+                {/* <div className="flex items-center mt-2.5 mb-5">
                     <svg
                         className="w-4 h-4 text-yellow-300 mr-1"
                         aria-hidden="true"
@@ -61,14 +61,14 @@ export default function Card() {
                     <span className="bg-blue-100 text-blue-800 text-xs font-semibold mr-2 px-2.5 py-0.5 rounded dark:bg-blue-200 dark:text-blue-800 ml-3">
                         4.0
                     </span>
-                </div>
+                </div> */}
                 <div className="flex items-center justify-between">
-                    <span className="text-3xl font-bold text-gray-900 dark:text-white">$599</span>
+                    <span className="text-3xl font-bold text-gray-900 dark:text-white">Adarsh Chaudhary</span>
                     <a
                         href="/"
                         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                     >
-                        Add to cart
+                        More Trips
                     </a>
                 </div>
             </div>
---
+++ 09themeSwitcher/src/components/Card1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Card1() {
+  return (
+    <div>
+         <h1>Adarsh Chaudhary</h1>
+    </div>
+  )
+}
